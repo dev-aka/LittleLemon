f2b10e3fbf3de5672ddcc600b93b0beac035d1d2
ANSAR K A <developer.ka.in@gmail.com>
2025-11-09 21:52:02 -0400

    committing secure table booking api

--- LittleLemon/demoproject/demoproject/urls.py
+++ LittleLemon/demoproject/demoproject/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#import obtain_auth_token view
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #add following lines to update urlpatterns list
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token)
 ]
